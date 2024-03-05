533b050ac1187cfa7627b39217be93b2b469b50f
Sim <sk325@st-andrews.ac.uk>
2024-03-05 16:14:33 +0000

    action listner

--- Frame.java
+++ Frame.java
@@ -2,10 +2,15 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Frame extends JFrame {
 
@@ -32,17 +37,32 @@ public class Frame extends JFrame {
         mainPanel.add(buttonPanel);
 
         JButton save = new JButton("Save");
+        final JLabel saveLabel = new JLabel();
         JButton reload = new JButton("Reload");
     
         buttonPanel.add(save);
+        buttonPanel.add(saveLabel);
         buttonPanel.add(reload);
         
         save.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+                JFileChooser fileChooser = new JFileChooser();
+                FileNameExtensionFilter extensionFilter = new FileNameExtensionFilter("Game of Life Files", "gol");
+                fileChooser.setFileFilter(extensionFilter);
+                int option = fileChooser.showSaveDialog(Frame.this);
+                if (option == JFileChooser.APPROVE_OPTION) {
+                    File file = fileChooser.getSelectedFile();
+
+                    // check to see if the file ends with ".gol" by extracting the absolute path and appending it
+                    if (!file.getAbsolutePath().endsWith(".gol")) {
+                        file = new File(file + ".gol");
+                    }
+                    saveLabel.setText("File saved as: " + file.getName());
+                } else {
+                    saveLabel.setText("Save command did not work");
+                }
             }
             
         });
