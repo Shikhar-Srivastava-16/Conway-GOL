37d4652b97e6bde93279530e2b53437c5c791344
Shikhar Srivastava <109688678+Shikhar-Srivastava-16@users.noreply.github.com>
2024-03-05 16:07:03 +0000

    added empty methods for saves

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
 public class Main {
 
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 
+     */
+    public static void saveGame() {
+    }
+
+    /**
+     * 
+     */
+    public static void loadSave() {
+
+    }
 }
