7b3c0fbd1d9f57523e3b60b8fad70ba672cd685f
Shikhar Srivastava <109688678+Shikhar-Srivastava-16@users.noreply.github.com>
2024-03-05 16:31:06 +0000

    made changes to main to add array

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
-    private static Cell[][] arrCells;
+    private static Cell[][] arrCells = new Cell[50][50];
     public static void main(String[] args) {
 
     }
 
     /**
+     * @throws IOException
+     * 
      * 
      */
-    public static void saveGame() {
+    public static void saveGame(File saveFile) throws IOException {
+        FileWriter writerObj = new FileWriter(saveFile);
         for (Cell[] row : arrCells) {
+            for (Cell cell : row) {
+                // for live cell
+                if (cell.isLive())
+                    writerObj.write("o");
+                else
+                    writerObj.write(".");
+                // System.out.println(); for logs
+            }
         }
     }
 
@@ -17,6 +32,18 @@ public class Main {
      * 
      */
     public static void loadSave() {
-
+        /*
+         * FileWriter writerObj = new FileWriter(saveFile);
+         * for (Cell[] row : arrCells) {
+         * for (Cell cell : row) {
+         * // for live cell
+         * if (cell.isLive())
+         * writerObj.write("o");
+         * else
+         * writerObj.write(".");
+         * // System.out.println(); for logs
+         * }
+         * }
+         */
     }
 }
